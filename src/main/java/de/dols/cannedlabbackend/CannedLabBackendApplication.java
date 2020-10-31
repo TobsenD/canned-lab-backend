aadb9425e0b0c7b42bc17577307b6b13e1096d9f
Tobias Dols <tobias.dols@outlook.com>
2020-10-31 16:05:13 +0100

    Initial commit

---
+++ src/main/java/de/dols/cannedlabbackend/CannedLabBackendApplication.java
@@ -0,0 +1,13 @@
+package de.dols.cannedlabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CannedLabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CannedLabBackendApplication.class, args);
+	}
+
+}
